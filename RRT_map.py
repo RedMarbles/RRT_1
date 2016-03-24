37d8881fc9fbac070dc5c6a01d19a7d4aee843ae
RedMarbles <anoopvargheese@gmail.com>
2016-03-24 19:57:26 +0100

    Tested basic state update eqn, successful

--- RRT_map.py
+++ RRT_map.py
@@ -8,7 +8,35 @@ from matplotlib import pyplot as plt
 class Map:
 	"""Represents the full state and interface of the map"""
 
-	def __init__(self):
-		# Do stuff
+	map_limits = {	'xmin':-5, 
+					'xmax':+5, 
+					'ymin':-5, 
+					'ymax':+5  }	# Limits of the map
+	default_params = { 	'bot_length':0.5, 
+						'bot_color':'b',
+						'path_color':'b' }
 
-	def addRectObstacle(self, ...)
+	def __init__(self):
+		""" Map class Constructor """
+		#TODO:
+		# Choose to update default values for the following:
+		# * map_limits
+
+	def addRectObstacle(self, ...):
+		""" Adds a rectangular obstacle to the map """
+		#TODO
+
+	def drawTreeMap(self, states_tree, draw_params=None):
+		""" Accepts tree of states, and outputs the map with obstacles and the accepted states 
+		    states_tree : Tree object 
+		    draw_params : Dictionary that holds the """
+		# Merge the default parameters with the argument
+		params = default_params.copy()
+		if(draw_params!=None):
+			params.update(draw_params)
+		##
+
+		#TODO : compelte this function
+
+	def drawStatesMap(self, states_list, draw_params=None):
+		#TODO
--- test.py
+++ test.py
@@ -7,15 +7,21 @@ from matplotlib import pyplot as plt
 
 X_list = []
 U_list = []
-dt = 0.4
+dt = 0.8
+map_border = { 'xmin':-5, 'xmax':+5, 'ymin':-5, 'ymax':+5}
+dl = 0.4 # length of orientation marker
 
 def state_update(v,omega):
 	"Tests the simple delta_X formula"
 
+	global X_list 
+	global U_list 
+	global dt
+
 	X = X_list[ len(X_list) - 1 ] #Get last state
 	x = X[0]
 	y = X[1]
-	theta = X[3]
+	theta = X[2]
 
 	if( omega != 0 ):
 		dx =  v/omega*( np.sin(theta + omega*dt) - np.sin(theta) )
@@ -29,12 +35,18 @@ def state_update(v,omega):
 	X_list.append( (x+dx, y+dy, theta+dtheta) )
 	U_list.append( (v,omega) )
 
-	redraw()
+	#redraw()
 	#end function state_update()
 
 
 def redraw():
 	"Redraws the plots"
+
+	global X_list 
+	global U_list 
+	global dt
+	global map_border
+	global dl
 
 	f1 = plt.figure(1)
 	#f2 = plt.figure(2)
@@ -48,17 +60,22 @@ def redraw():
 		x2 = X2[0]
 		y2 = X2[1]
 		theta2 = X2[2]
-		f1.plot([x1,x2],[y1,y2],"bo") #plot squares
-		f1.plot([x1,x2],[y1,y2],"b") #plot lines
-		f1.plot([x1,x1+np.cos(theta1)],[y1,y1+np.sin(theta1),"r"]) #red orientation marker
-		f1.plot([x2,x2+np.cos(theta2)],[y2,y2+np.sin(theta2),"r"]) #red orientation marker
+		plt.plot([x1,x2],[y1,y2],"bo") #plot squares
+		plt.plot([x1,x2],[y1,y2],"b") #plot lines
+		plt.plot([x1,x1+dl*np.cos(theta1)],[y1,y1+dl*np.sin(theta1)],"r") #red orientation marker
+		plt.plot([x2,x2+dl*np.cos(theta2)],[y2,y2+dl*np.sin(theta2)],"r") #red orientation marker
 
-	f1.show()
+	plt.axis([map_border['xmin'], map_border['xmax'], map_border['ymin'], map_border['ymax']])
+	plt.show()
 	#end function redraw()
 
 
 def init(x0, y0, theta0):
 	"Reinitialize state"
+
+	global X_list 
+	global U_list 
+	global dt
 
 	X_list = [(x0,y0,theta0)]
 	U_list = [(0,0)]
@@ -66,6 +83,7 @@ def init(x0, y0, theta0):
 
 if __name__ == '__main__':
 	init(0,0,0)
-	state_update(2,0)
+	state_update(2,-1)
 	state_update(1,1)
 	state_update(2,4)
+	redraw()
